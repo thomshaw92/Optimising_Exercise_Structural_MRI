06491a799b53237894d0ee8a965112154bd1efa3
thomshaw92 <thomasbasilshaw@gmail.com>
2020-05-18 01:11:25 +1000

    various deletions of old unused scripts

---
+++ OPTIMEX_github/2_tse_mc/README.md
@@ -0,0 +1,7 @@
+These scripts were from an earlier version
+
+
+The Non Linear MoCo was incorporated into the preprocessing script
+
+
+These can be safely ignored
--- OPTIMEX_github/2_tse_mc/tse_mc_nlin_submission_script_ses-06.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for subjName in sub-1194JS sub-1198GS sub-1215AH sub-1135CD sub-1128LW ; do 
-	qsub -v SUBJNAME=$subjName ~/scripts/OPTIMEX/2_tse_mc/tse_mc_nlin_pbs_script_ses-06.pbs
-done
