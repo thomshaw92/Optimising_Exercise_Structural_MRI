3671e771174eb524ff0a45927a7cb17d617c0e9b
thomshaw92 <thomasbasilshaw@gmail.com>
2020-06-08 10:53:48 +1000

    include new freesurfer scripts

--- OPTIMEX_github/9_LASHiS/LASHiS_submission_script.sh
+++ OPTIMEX_github/9_LASHiS/LASHiS_submission_script.sh
@@ -2,7 +2,7 @@
 github_dir=~/scripts/Optimising_Exercise_Structural_MRI/OPTIMEX_github/
 
 for subjName in `cat ${github_dir}/subjnames_01_only.csv` ; do
-       qsub -v SUBJNAME=$subjName ${github_dir}/9_LASHiS/OPTIMEX_ASHS_pbs_script_1.pbs
+       qsub -v SUBJNAME=$subjName ${github_dir}/9_LASHiS/OPTIMEX_ASHS_pbs_script_1_ses.pbs
 done
 for subjName in `cat ${github_dir}/subjnames_06_only.csv` ; do
         qsub -v SUBJNAME=$subjName ${github_dir}/9_LASHiS/OPTIMEX_LASHiS_pbs_script_2_ses.pbs
