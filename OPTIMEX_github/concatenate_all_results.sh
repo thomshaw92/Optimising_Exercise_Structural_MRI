3ffa7f43a376503f44b1851ba5c6a189e9e6e2cb
thomshaw92 <thomasbasilshaw@gmail.com>
2020-06-13 10:05:54 +1000

    add 3 and 5 timepoints

--- OPTIMEX_github/concatenate_all_results.sh
+++ OPTIMEX_github/concatenate_all_results.sh
@@ -30,5 +30,54 @@ for subjName in `cat ${github_dir}/subjnames_06_only.csv` ; do
 done
 #ses-03 only
 for subjName in `cat ${github_dir}/subjnames_12_only.csv` ; do
+    mkdir ${results_dir}/${subjName}
+    if [[ -e ${proc_lashis_dir}/${subjName}/LASHiS/${subjName}_ses-01_T1w_N4corrected_norm_preprocleftSSTLabelsWarpedToTimePoint0_stats.txt ]] ; then
+        
+        cp ${proc_lashis_dir}/${subjName}/LASHiS/*txt ${results_dir}/${subjName}/
+        
+        paste -d' ' ${results_dir}/${subjName}/${subjName}*ses-01*left*.txt \
+        ${results_dir}/${subjName}/${subjName}*ses-02*left*.txt \
+        ${results_dir}/${subjName}/${subjName}*ses-03*left*.txt \
+        > ${results_dir}/${subjName}/${subjName}_left_3-sessions.txt
+        
+        paste -d' ' ${results_dir}/${subjName}/${subjName}*ses-01*right*.txt \
+        ${results_dir}/${subjName}/${subjName}*ses-02*right*.txt \
+        ${results_dir}/${subjName}/${subjName}*ses-03*right*.txt \
+        > ${results_dir}/${subjName}/${subjName}_right_3-sessions.txt
+        
+        
+        cat ${results_dir}/${subjName}/${subjName}_left_3-sessions.txt>>${results_dir}/hippocampus_volumes.csv
+        cat ${results_dir}/${subjName}/${subjName}_right_3-sessions.txt>>${results_dir}/hippocampus_volumes.csv
+        cd ${proc_lashis_dir}
+        echo "tar -cvzf ${subjName}.tar.gz ./${subjname} "
+    fi
+done
+#ses-05 only
+for subjName in `cat ${github_dir}/subjnames_24_only.csv` ; do
+    mkdir ${results_dir}/${subjName}
+    if [[ -e ${proc_lashis_dir}/${subjName}/LASHiS/${subjName}_ses-01_T1w_N4corrected_norm_preprocleftSSTLabelsWarpedToTimePoint0_stats.txt ]] ; then
+        
+        cp ${proc_lashis_dir}/${subjName}/LASHiS/*txt ${results_dir}/${subjName}/
+        
+        paste -d' ' ${results_dir}/${subjName}/${subjName}*ses-01*left*.txt \
+        ${results_dir}/${subjName}/${subjName}*ses-02*left*.txt \
+        ${results_dir}/${subjName}/${subjName}*ses-03*left*.txt \
+        ${results_dir}/${subjName}/${subjName}*ses-04*left*.txt \
+        ${results_dir}/${subjName}/${subjName}*ses-05*left*.txt \
+        > ${results_dir}/${subjName}/${subjName}_left_3-sessions.txt
+        
+        paste -d' ' ${results_dir}/${subjName}/${subjName}*ses-01*right*.txt \
+        ${results_dir}/${subjName}/${subjName}*ses-02*right*.txt \
+        ${results_dir}/${subjName}/${subjName}*ses-03*right*.txt \
+        ${results_dir}/${subjName}/${subjName}*ses-04*right*.txt \
+        ${results_dir}/${subjName}/${subjName}*ses-05*right*.txt \
+        > ${results_dir}/${subjName}/${subjName}_right_3-sessions.txt
+        
+        
+        cat ${results_dir}/${subjName}/${subjName}_left_3-sessions.txt>>${results_dir}/hippocampus_volumes.csv
+        cat ${results_dir}/${subjName}/${subjName}_right_3-sessions.txt>>${results_dir}/hippocampus_volumes.csv
+        cd ${proc_lashis_dir}
+        echo "tar -cvzf ${subjName}.tar.gz ./${subjname} "
+    fi
 done
 date
