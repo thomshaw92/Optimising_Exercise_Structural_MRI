84afa9d3bc3fdf4934f0ffb8b3ef0485165cc52d
thomshaw92 <thomasbasilshaw@gmail.com>
2020-06-23 00:42:21 +1000

    finalised script

--- OPTIMEX_github/9_LASHiS/LASHiS_submission_script.sh
+++ OPTIMEX_github/9_LASHiS/LASHiS_submission_script.sh
@@ -3,7 +3,7 @@ github_dir=~/scripts/Optimising_Exercise_Structural_MRI/OPTIMEX_github/
 
 for subjName in `cat ${github_dir}/subjnames_01_only.csv` ; do
        qsub -v SUBJNAME=$subjName ${github_dir}/9_LASHiS/OPTIMEX_ASHS_pbs_script_1_ses.pbs
-done
+ done
 for subjName in `cat ${github_dir}/subjnames_06_only.csv` ; do
         qsub -v SUBJNAME=$subjName ${github_dir}/9_LASHiS/OPTIMEX_LASHiS_pbs_script_2_ses.pbs
 done
@@ -11,5 +11,5 @@ for subjName in `cat ${github_dir}/subjnames_12_only.csv` ; do
       qsub -v SUBJNAME=$subjName ${github_dir}/9_LASHiS/OPTIMEX_LASHiS_pbs_script_3_ses.pbs
 done
 for subjName in `cat ${github_dir}/subjnames_24_only.csv` ; do 
-	qsub -v SUBJNAME=$subjName ${github_dir}/9_LASHiS/OPTIMEX_LASHiS_pbs_script_5_ses.pbs
+	qsub -v SUBJNAME=$subjName ${github_dir}/9_LASHiS/OPTIMX_LASHiS_pbs_script_5_ses.pbs
 done
