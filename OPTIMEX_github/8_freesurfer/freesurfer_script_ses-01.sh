325f9075c323d8940365f81204418d0f59b2b47a
thomshaw92 <thomasbasilshaw@gmail.com>
2020-07-20 17:45:42 +1000

    Updates to new FS version. ETIV calculation only

--- OPTIMEX_github/8_freesurfer/freesurfer_script_ses-01.sh
+++ OPTIMEX_github/8_freesurfer/freesurfer_script_ses-01.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
-#Thomas Shaw 24/9/18 
-#Freesurfer script for HPC
-subjName=$1
+#Thomas Shaw 24/9/18
+#Freesurfer script for HPC - only for TP1 for ETIV measurement.
+subjName=sub-1001DS
 source ~/.bashrc
-module load singularity/2.5.1
+module load singularity
 ss="ses-01"
-singularity="singularity exec --bind $TMPDIR:/TMPDIR --pwd /TMPDIR/ /30days/$USER/ants_fsl_robex_20180427.simg"
-rsync -r -v /RDS/Q0535/optimex/data/${subjName}/$ss/anat/${subjName}_${ss}_T1w.nii.gz $TMPDIR 
-rsync -r -v /RDS/Q0535/optimex/data/derivatives/preprocessing/$subjName/${subjName}_${ss}_T2w_NlinMoCo_res-iso.3_N4corrected_denoised_norm_brain_preproc.nii.gz $TMPDIR/
+singularity="singularity exec --bind $TMPDIR:/TMPDIR --pwd /TMPDIR/ /30days/$USER/ants_fsl_robex_20180524.simg"
+#rsync -r -v /3/data/bids/${subjName}/$ss/anat/${subjName}_${ss}*T1w.nii.gz $TMPDIR/${subjName}_${ss}_T1w.nii.gz
+#rsync -r -v /RDS/Q0535/optimex/data/derivatives/preprocessing/$subjName/${subjName}_${ss}_T2w_NlinMoCo_res-iso.3_N4corrected_denoised_norm_brain_preproc.nii.gz $TMPDIR/
 cd $TMPDIR
 chmod -R 740 $TMPDIR/
-t1w=$TMPDIR/${subjName}_${ss}_T1w.nii.gz
-T2=$TMPDIR/${subjName}_${ss}_T2w_NlinMoCo_res-iso.3_N4corrected_denoised_norm_brain_preproc.nii.gz
+cp /30days/uqtshaw/optimex/bids/${subjName}/ses-01/anat/${subjName}_${ss}*run-1_T1w.nii.gz $TMPDIR/${subjName}_t1w.nii.gz
+t1w=/30days/uqtshaw/optimex/bids/${subjName}/ses-01/anat/${subjName}*${ss}_T1w.nii.gz
+#T2=$TMPDIR/${subjName}_${ss}_T2w_NlinMoCo_res-iso.3_N4corrected_denoised_norm_brain_preproc.nii.gz
 module load freesurfer/6.1dev
 source $FREESURFER_HOME/SetUpFreeSurfer.sh
-SUBJECTS_DIR=$TMPDIR/
-recon-all -autorecon1 -cm -s $subjName -i $t1w
+SUBJECTS_DIR=$TMPDIR
+recon-all -autorecon1 -cm -s $subjName -i $TMPDIR/${subjName}_t1w.nii.gz
 mri_convert $SUBJECTS_DIR/${subjName}/mri/brainmask.mgz $SUBJECTS_DIR/${subjName}/mri/brainmask_nii.nii.gz
-$singularity runROBEX.sh ${subjName}_${ss}_T1w.nii.gz ${subjName}_${ss}_T1w_brain_preproc.nii.gz ${subjName}_${ss}_T1w_brainmask.nii.gz
-$singularity flirt -searchrx -180 180 -searchry -180 180 -searchrz -180 -180 -in ${subjName}_${ss}_T1w_brain_preproc.nii.gz -ref $subjName/mri/brainmask_nii.nii.gz -out ${subjName}_t1_brainmask_robex.nii.gz
-mri_convert $TMPDIR/${subjName}_t1_brainmask_robex.nii.gz $SUBJECTS_DIR/${subjName}/mri/brainmask.mgz
-recon-all -autorecon2 -autorecon3 -cm -s $subjName -T2 $T2 -T2pial -no-isrunning
-segmentHA_T2.sh $subjName $T2 T1T2 USE_T1 [$SUBJECTS_DIR]
+$singularity runROBEX.sh /TMPDIR/${subjName}/mri/brainmask_nii.nii.gz /TMPDIR/${subjName}/mri/brain_robex.nii.gz /TMPDIR/${subjName}/mri/brainmask_robex.nii.gz
+mri_convert $SUBJECTS_DIR/${subjName}/mri/brain_robex.nii.gz $SUBJECTS_DIR/${subjName}/mri/brainmask.mgz
+recon-all -autorecon2 -autorecon3 -cm -s $subjName
+#segmentHA_T2.sh $subjName $T2 T1T2 USE_T1 [$SUBJECTS_DIR]
 
-chmod -R 777 $TMPDIR 
-rsync -v -c -r $TMPDIR/$subjName /RDS/Q0535/optimex/data/derivatives/5_freesurfer/
+chmod -R 777 $TMPDIR
+rsync -v -c -r $TMPDIR/$subjName /90days/${USER}/optimex/freesurfer/
--- OPTIMEX_github/8_freesurfer/freesurfer_submission_script_ses-01.sh
+++ OPTIMEX_github/8_freesurfer/freesurfer_submission_script_ses-01.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-for subjName in `cat /30days/$USER/subjnames_ses-01.csv` ; do 
+github_dir=~/scripts/Optimising_Exercise_Structural_MRI/OPTIMEX_github/
+for subjName in `cat ${github_dir}/subjnames_01_only.csv` ; do 
 	qsub -v SUBJNAME=$subjName ~/scripts/OPTIMEX/8_freesurfer/freesurfer_pbs_script_ses-01.pbs
 done
